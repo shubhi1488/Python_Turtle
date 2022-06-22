f9218c69dd9eeb83fa9621a4dd7c0f7a524c9dad
Shubhi Agarwal <84718320+shubhi1488@users.noreply.github.com>
2022-06-22 23:07:12 +0530

    Create GoTo.py

---
+++ GoTo.py
@@ -0,0 +1,12 @@
+#move the turtle to another area using .goto
+import turtle
+wn=turtle.Screen()
+wn.bgcolor("coral")
+s=turtle.Turtle()
+s.goto(100,100)
+s.goto(-100,100)
+s.goto(-100,100)
+s.goto(100,-100)
+s.goto(-100,-100)
+s.goto(0,0)
+turtle.done()
