c2f913f7fd997b3e07133d90a89f74a7ab193395
Shubhi Agarwal <84718320+shubhi1488@users.noreply.github.com>
2022-06-22 22:58:51 +0530

    Create MovingTurtle.py
    moving the turtle

---
+++ MovingTurtle.py
@@ -0,0 +1,10 @@
+#moving the turtle in right(90),forward(100),left(90),backward(100).
+import turtle
+wn=turtle.Screen()
+wn.bgcolor("coral")
+s=turtle.Turtle()
+s.right(90)
+s.forward(100)
+s.left(90)
+s.backward(100)
+turtle.done()
